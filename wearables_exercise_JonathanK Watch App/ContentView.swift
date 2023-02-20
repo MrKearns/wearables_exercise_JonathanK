1bfea043f58acdd049b40a9f0d0300ae4e596fb6
MrKearns <jkearns631@students.mchenry.edu>
2023-02-20 09:41:11 -0600

    first

--- wearables_exercise_JonathanK Watch App/ContentView.swift
+++ wearables_exercise_JonathanK Watch App/ContentView.swift
@@ -8,12 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var titleText = Text("Flip the Coin")
+            
+
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+           
+            
+            titleText
+//                .font(.largeTitle)
+                .foregroundColor(Color.gray)
+                .accessibilityLabel("flipTheCoin")
+                .multilineTextAlignment(.center)
+                .font(.system(size: 35))
+            
+            Spacer()
+            Button {
+                
+                
+                titleText = Text("Flipping.")
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    titleText = Text("Flipping..")
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        titleText = Text("Flipping...")
+                    }
+                    
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                            
+                            let randomFlip = Bool.random()
+                            
+                            if randomFlip == true{
+                                titleText = Text(Image(systemName: "brain.head.profile"))
+                                
+                            } else{
+                                
+                                titleText = Text(Image(systemName:"building.columns"))
+                                
+            
+                            }
+                        
+                        }
+                }
+                //do something
+                
+                
+            } label: {
+                Text("Flip")
+                    .foregroundColor(.white)
+                    
+            }
+            .background(Color.blue)
+            .cornerRadius(20)
+            .accessibilityIdentifier("flipTheCoin")
+            
         }
         .padding()
     }
